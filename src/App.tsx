84f789661159e3bb3988dd3326f8dc122bd72790
shazkhan2 <seewardak@hotmail.com>
2024-06-02 16:52:14 +0200

    Navigation bar created

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar"
+
 function App() {
 
   return <div>
+    <Navbar/>
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/store" element={<Store />} />
